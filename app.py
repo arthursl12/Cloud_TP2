8eb40732d01a5c92d62fb30936820e4a7212d193
arthursl12 <arthursoutolima@gmail.com>
2022-06-05 14:06:51 -0300

    Flask with date

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import request
 
+import datetime
+import pickle
+
 app = Flask(__name__)
-app.model = pickle.load(open("*.sk", "rb"))
+app.model = pickle.load(open("trained_model.sk", "rb"))
+app.last_updated = pickle.load(open("last_updated.date", "rb"))
 
 @app.route("/")
 def hello_world():
@@ -13,12 +17,13 @@ def american_process():
     # Get input JSON
     text = request.get_json(force=False).text
     
-    
-    
+    date_string = datetime.strptime(
+        app.last_updated, '%Y-%m-%dT%H:%M:%S%z'
+    ).isoformat()
     # Send a dict response (Flask will convert to JSON
     return {
         "is_american": 1,
         "version": "0.0",
-        "model_date": 
+        "model_date": date_string,
     }
 
